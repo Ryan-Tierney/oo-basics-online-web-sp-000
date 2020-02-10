3e1cffb5b3257e0ee271aefc804be9fc7c934e92
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-10 17:21:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,49 +1,12 @@
-class Shoe
-  def initialize(brand, color, size, material, condition)
-    @brand = brand
-    @color = color
-    @size = size 
-    @material = material 
-    @condition = condition
+class Shoe 
+  def initialize(brand)
+    @brand = brand 
   end 
-   
+  
   def brand=(brand)
     @brand = brand 
   end 
   
-  def brand
+  def brand 
     @brand 
   end 
-  
-  def color=(color)
-    @color = color 
-  end 
-  
-  def color 
-    @color
-  end 
-  
-  def size=(size)
-    @size = size  
-  end 
-  
-  def size 
-    @size 
-  end 
-  
-  def material=(material)
-    @material = material 
-  end 
-  
-  def material 
-    @material 
-  end 
-  
-  def condition=(condition)
-    @condition = condition 
-  end 
-  
-  def condition
-    @condition
-  end 
-end 
