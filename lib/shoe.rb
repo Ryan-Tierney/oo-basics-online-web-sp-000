5110085b7fa8f9a770799bd6d1f8c6558eaef29b
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-10 17:45:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,9 +13,4 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end 
-  
-  def cobble()
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
 end 
