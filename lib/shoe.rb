fc8399db1e1078c2dd9dddba2b5ac2efc4db1d94
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-10 17:15:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,19 +11,31 @@ class Shoe
     @brand 
   end 
   
+  def color=(color)
+    @color = color 
+  end 
+  
   def color 
     @color 
+  
+  def size=(size)
+    @size = size  
   end 
   
   def size 
     @size 
+  
+  def material=(material)
+    @material = material 
   end 
   
   def material 
     @material 
+  
+  def condition=(condition)
+    @condition = condition 
   end 
   
-  def condition 
-    @condition 
-  end 
+  def condition
+    @condition
 end 
