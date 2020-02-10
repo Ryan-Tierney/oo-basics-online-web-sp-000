7a8ad07ae9062ca11ad56742fea63b71510c9605
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-10 17:42:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,37 +7,7 @@ class Shoe
     @brand 
   end 
   
-  def color=(color)
-    @color = color
-  end 
-  
-  def color
-    @color
-  end 
-  
-  def size=(size)
-    @size = size
-  end 
-  
-  def size 
-    @size
-  end
-  
-  def material=(material)
-    @material = material 
-  end 
-  
-  def material
-    @material
-  end 
-  
-  def condition=(condition)
-    @condition = condition
-  end 
-  
-  def condition
-    @condition
-  end 
+ attr_accessor :color, :size, :codition, :material,  
   
   def cobble 
     puts "Your shoe is as good as new!"
