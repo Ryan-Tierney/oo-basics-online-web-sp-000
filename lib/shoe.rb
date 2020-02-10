7059a11c0abed518eb8148626ae00d46a6ce6b5d
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-10 17:10:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand 
+  end 
+   
+  def brand=(brand)
+    @brand = brand 
+  end 
+  
+  def brand
+    @brand 
+  end 
+  
+  def color 
+    @color 
+  end 
+  
+  def size 
+    @size 
+  end 
+  
+  def material 
+    @material 
+  end 
+  
+  def condition 
+    @condition 
+  end 
